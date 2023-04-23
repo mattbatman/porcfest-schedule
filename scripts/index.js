cc154d8e57fa7c973a075d669a37def1162bc574
Matt Batman <batman.matt@gmail.com>
2023-04-23 12:43:22 -0400

    Start deconstructing ical file data

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,30 @@
 const fs = require('fs');
 const ICAL = require('ical.js');
 
-function generateData() {
-  fs.readFile(
-    './scripts/PorcfestOfflineSchedule.ics',
-    'utf8',
-    function (err, data) {
-      if (err) {
-        throw err;
-      }
+const ICS_FILE = './scripts/PorcfestOfflineSchedule.ics';
 
-      const jcalData = ICAL.parse(data);
-      console.log(jcalData);
+function generateData() {
+  fs.readFile(ICS_FILE, 'utf8', function (err, data) {
+    if (err) {
+      throw err;
     }
-  );
+
+    const [metaString, metaMisc, eventData] = ICAL.parse(data);
+
+    eventData.forEach(function ([string, list]) {
+      const [
+        uidList,
+        summaryList,
+        descriptionList,
+        locationList,
+        startDateTimeList,
+        endDateTimeList,
+        timestampList
+      ] = list;
+
+      console.log(summaryList);
+    });
+  });
 }
 
 generateData();
