f71a0a1cc4dbbe19511ea84a8c13789d6682f852
Matt Batman <batman.matt@gmail.com>
2023-04-23 21:27:47 -0400

    Add script to write ics file data as JSON

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const ICAL = require('ical.js');
 
 const ICS_FILE = './scripts/PorcfestOfflineSchedule.ics';
+const OUTPUT_FILE = './src/data/schedule-2023.json';
 
 function generateData() {
   fs.readFile(ICS_FILE, 'utf8', function (err, data) {
@@ -11,19 +12,40 @@ function generateData() {
 
     const [metaString, metaMisc, eventData] = ICAL.parse(data);
 
-    eventData.forEach(function ([string, list]) {
+    const jsonData = eventData.map(function ([string, list]) {
       const [
         uidList,
         summaryList,
         descriptionList,
         locationList,
-        startDateTimeList,
-        endDateTimeList,
+        startDatetimeList,
+        endDatetimeList,
         timestampList
       ] = list;
 
-      console.log(summaryList);
+      return {
+        summary: summaryList[3],
+        description: descriptionList[3],
+        location: locationList[3],
+        startDatetime: startDatetimeList[3],
+        endDatetimeList: endDatetimeList[3],
+        timestamp: timestampList[3]
+      };
     });
+
+    writeJson(jsonData);
+  });
+}
+
+function writeJson(data) {
+  fs.writeFile(OUTPUT_FILE, JSON.stringify(data), function (err) {
+    if (err) {
+      console.log('Failed to write!');
+      console.log(err);
+      return;
+    }
+
+    console.log('Done writing schedule JSON.');
   });
 }
 
