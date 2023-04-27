53d6c0bf536e91ddd8bce44ecb2d377038b75128
Matt Batman <batman.matt@gmail.com>
2023-04-26 21:48:49 -0400

    Attempt CSS include

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
+const isProd = process.env.NODE_ENV === 'production';
+const repo = '/porcfest-schedule'
+const basePath = isProd ? repo : '';
+const assetPrefix = isProd ? repo : '';
+
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
   output: 'export',
+  basePath,
+  assetPrefix
 }
 
 module.exports = nextConfig
