ed844c383c9fc6da2fe3dc9592a2f48eacac9364
Matt Batman <batman.matt@gmail.com>
2023-04-26 22:21:13 -0400

    Remove next.js file prefix tampering

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
-const isProd = process.env.NODE_ENV === 'production';
-const repo = '/porcfest-schedule'
-const basePath = isProd ? repo : '';
-const assetPrefix = isProd ? repo : '';
-
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
   output: 'export',
-  basePath,
-  assetPrefix
 }
 
 module.exports = nextConfig
