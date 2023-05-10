3b2923f2b9afc2af0d38bae0459f3a71fcf60f79
Matt Batman <batman.matt@gmail.com>
2023-05-09 22:27:37 -0400

    Add meta tag for responsive scaling

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Head from 'next/head';
 import AppProvider from '../contexts/AppProvider';
 import '../styles/global.css';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <AppProvider>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" key="responsive" />
+      </Head>
       <Component {...pageProps} />
     </AppProvider>
   );
