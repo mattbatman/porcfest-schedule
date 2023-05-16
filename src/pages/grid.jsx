adf8abf19ec70143cd030a1c2b0235542024483c
Matt Batman <batman.matt@gmail.com>
2023-05-16 19:18:30 -0400

    Update data

--- src/pages/grid.jsx
+++ src/pages/grid.jsx
@@ -29,7 +29,7 @@ const GridPage = () => {
       <title>PorcFest Schedule 2023 | Grid</title>
       <h1>PorcFest Schedule 2023</h1>
       <p>
-        Last updated May 9, 2023. This site does not sync in real time with the
+        Last updated May 16, 2023. This site does not sync in real time with the
         official <a href="https://porcfest.com/schedule/">PorcFest schedule</a>.
       </p>
 
